2e411daac1c1d43e1d9f0ac31388338ca6edd358
Lakshay983 <56173382+Lakshay983@users.noreply.github.com>
2025-10-14 19:27:21 -0500

    add start of react

---
+++ my-projects-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Projects from './Projects';
+
+function App() {
+  return (
+    <div style={{padding: 20, fontFamily: 'Inter, Arial, sans-serif'}}>
+      <Projects />
+    </div>
+  );
+}
+
+export default App;
